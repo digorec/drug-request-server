72351723a0462077e9c69483dae16bbb7c94b52f
Sergey Kanukov <s.kanukov@gmail.com>
2017-01-26 12:49:11 +0300

    Add profile tab to user

--- plugins/macrobit/drugreq/Plugin.php
+++ plugins/macrobit/drugreq/Plugin.php
@@ -1,6 +1,10 @@
 <?php namespace Macrobit\Drugreq;
 
+use October\Rain\Support\Facades\File;
+use October\Rain\Support\Facades\Yaml;
 use System\Classes\PluginBase;
+use RainLab\User\Models\User as UserModel;
+use RainLab\User\Controllers\Users as UsersController;
 
 class Plugin extends PluginBase
 {
@@ -10,5 +14,26 @@ class Plugin extends PluginBase
 
     public function registerSettings()
     {
+    }
+
+    public function boot()
+    {
+        parent::boot();
+
+        $this->extendUsersController();
+    }
+
+    private function extendUsersController()
+    {
+        UsersController::extendFormFields(function ($widget) {
+            // Prevent extending of related form instead of the intended User form
+            if (!$widget->model instanceof UserModel) {
+                return;
+            }
+
+            $configFile = __DIR__ . '/models/user/fields.yaml';
+            $config = Yaml::parse(File::get($configFile));
+            $widget->addTabFields($config);
+        });
     }
 }
--- plugins/macrobit/drugreq/lang/en/lang.php
+++ plugins/macrobit/drugreq/lang/en/lang.php
@@ -3,8 +3,14 @@
         'name' => 'DrugReq',
         'description' => 'A system to provide requests for drug purchasing',
         'lpu' => [
-            'name' => 'name',
-            'backend-menu-item-name' => 'Lpus',
+            'name' => 'Name',
+            'backend-menu-item-name' => 'LPU',
+        ],
+        'profile' => [
+            'lpu' => 'LPU'
+        ],
+        'tab' => [
+            'profile' => 'Profile',
         ],
     ],
 ];
