03b28fd89cc0fe810a153e6d2b5b722f82ade2f2
Sergey Kanukov <s.kanukov@gmail.com>
2017-01-31 09:50:50 +0300

    Set required fields while saving the request

--- plugins/macrobit/drugreq/controllers/Request.php
+++ plugins/macrobit/drugreq/controllers/Request.php
@@ -1,13 +1,16 @@
 <?php namespace Macrobit\Drugreq\Controllers;
 
 use Backend\Classes\Controller;
+use Backend\Facades\BackendAuth;
 use BackendMenu;
 use Illuminate\Support\Facades\Redirect;
+use Macrobit\Drugreq\Models\RequestCampaign;
 use Macrobit\Drugreq\Utils\RequestUtils;
 
 class Request extends Controller
 {
-    public $implement = ['Backend\Behaviors\ListController','Backend\Behaviors\FormController'];
+    public $implement = ['Backend\Behaviors\ListController',
+        'Backend\Behaviors\FormController'];
     
     public $listConfig = 'config_list.yaml';
     public $formConfig = 'config_form.yaml';
@@ -21,7 +24,6 @@ class Request extends Controller
     /**
      * Create Controller action
      * @param string $context Explicitly define a form context.
-     * @return void
      */
     public function create($context = null)
     {
@@ -31,5 +33,29 @@ class Request extends Controller
 
         // Call the FormController behavior update() method
         return $this->asExtension('FormController')->create($context);
+    }
+
+    /**
+     * Extend supplied model used by create and update actions, the model can
+     * be altered by overriding it in the controller.
+     * @param \Macrobit\Drugreq\Models\Request $model
+     * @return \Macrobit\Drugreq\Models\Request
+     */
+    public function formExtendModel($model)
+    {
+        // TODO: check if lpu and requestCampaign already exist.
+
+        // Check if user's lpu exists.
+        $user = BackendAuth::getUser();
+        $lpu = $user->lpu;
+        if (is_null($lpu)) {
+            return $model;
+        }
+
+        // Set required fields.
+        $requestCampaign = RequestCampaign::firstActiveNow();
+        $model->lpu = $lpu;
+        $model->requestCampaign = $requestCampaign;
+        return $model;
     }
 }
